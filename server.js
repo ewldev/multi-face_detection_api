aadc94c56fdcb0c2981dd2ee492c8836d2446aa8
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-03-16 15:40:42 -0400

    fixed old ref db on server.js

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> {
+   res.send("it's working!");
+});   
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
@@ -34,4 +36,4 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, ()=> {
   console.log('app is running on port ${process.env.PORT}');
-})
+});
